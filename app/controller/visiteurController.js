d4bd103731841e773aad4ec850c2bff5ddbaca59
Brieuc <brieuc.de-lantivy@epitech.eu>
2017-10-17 17:14:18 +0200

    Bloque la save de metiers de profils différents

--- app/controller/visiteurController.js
+++ app/controller/visiteurController.js
@@ -1,10 +1,42 @@
 module.exports = function (app, visiteurPersistence) {
-    /**
-     * Ajout d'un visiteur
-     */
     function callback(data)
     {
         return(data);
+    }
+
+    function check_profil(metier, profil) {
+        var fs = require('fs');
+        var json = JSON.parse(fs.readFileSync('./public/config.json', 'utf8'));
+        var obj = json;
+        var file = Object.keys(obj);
+        var i = 0;
+        var z = 1;
+        var j = 1;
+        var k = 0;
+        var my_profil;
+        var res = [];
+
+        my_profil = json[file[i]].split(',');
+        while (my_profil[0] != profil){
+            if (i > 0)
+                my_profil = json[file[i]].split(',');
+            i++;
+        }
+        if (i > 0)
+            i--;
+        while (my_profil[j]) {
+            my_profil[j] = my_profil[j].replace(/ /g, "_");
+            z = 0;
+            while (metier[z]) {
+                if (metier[z] == my_profil[j]) {
+                    res[k] = my_profil[j];
+                    k++;
+                }
+                z++;
+            }
+            j++;
+        }
+        return (res);
     }
 
     app.post('/adduser', function (req, res) {
@@ -28,6 +60,10 @@ module.exports = function (app, visiteurPersistence) {
             contact: req.body.ok,
             date: jour
         };
+        visiteur.metier = check_profil(req.body.metier, visiteur.profil);
+
+        if (req.body.ok != "ok")
+            visiteur.metier = "NULL";
         if (req.body.ok == "ok") {
             visiteur.contact = "oui";
             visiteurPersistence.save(visiteur, callback);
